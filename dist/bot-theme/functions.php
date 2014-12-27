16272df0cf4e6767f76772e678cead0c0e819876
Saqoosha <a@saqoo.sh>
2014-12-27 15:48:08 +0900

    Added custom child theme to output custom field for search engines

---
+++ dist/bot-theme/functions.php
@@ -0,0 +1,6 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+}
