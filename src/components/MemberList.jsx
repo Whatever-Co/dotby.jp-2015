e3fa0958c0065cebab3dd9dc4be885cc5ab89440
Saqoosha <a@saqoo.sh>
2018-01-09 12:28:49 +0900

    Shuffle member list

--- src/components/MemberList.jsx
+++ src/components/MemberList.jsx
@@ -110,7 +110,7 @@ module.exports = React.createClass({
     },
 
     componentDidMount() {
-        var members = _.keys(MEMBER_DATA).map((name) => {
+        var members = _.shuffle(_.keys(MEMBER_DATA)).map((name) => {
             return $.getJSON(`/wp-json/pages/members/${name}`, {lang: this.context.lang, _wp_json_nonce: window.nonce});
         });
         $.when.apply(null, members).done(()=> {
